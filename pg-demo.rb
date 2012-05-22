e886eb58048a09a89018db97744a5a85209665b9
Klaus Alfert <klaus.alfert@googlemail.com>
2012-05-22 15:00:03 +0200

    Minimales Ruby-Programm

---
+++ pg-demo.rb
@@ -0,0 +1,22 @@
+#
+# Demo Programm für PostgresQL in Ruby
+# 
+
+require 'postgres'
+
+PG_HOST = "localhost"
+PG_PORT = 5432
+PG_DB = "camp2012DB"
+PG_USER = "zeg"
+PG_PASSWORD = "geheim!"
+
+
+conn = PGconn.connect(PG_HOST, PG_PORT, '', '', PG_DB, PG_USER, PG_PASSWORD)
+
+res  = conn.exec('select tablename, tableowner from pg_tables')
+
+res.each do |row|
+  row.each do |column|
+   puts column
+  end
+end
